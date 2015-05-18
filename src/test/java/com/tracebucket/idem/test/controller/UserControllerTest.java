61b1abdf5ce025653ed0ed8a696bddfb97a950e9
Vishwajit <vishwajit.bhadrashetty@mmpsd.com>
2015-05-18 18:03:49 +0530

    All test cases for Group and User controller implemented successfully except testChangePassword.

--- src/test/java/com/tracebucket/idem/test/controller/UserControllerTest.java
+++ src/test/java/com/tracebucket/idem/test/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.tracebucket.idem.test.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tracebucket.idem.IdemStarter;
+import com.tracebucket.idem.domain.User;
 import com.tracebucket.idem.rest.resource.AuthorityResource;
 import com.tracebucket.idem.rest.resource.GroupResource;
 import com.tracebucket.idem.rest.resource.UserResource;
@@ -9,6 +10,7 @@ import com.tracebucket.idem.test.config.AccessTokenReceiverConfig;
 import com.tracebucket.idem.test.fixture.AuthorityResourceFixture;
 import com.tracebucket.idem.test.fixture.GroupResourceFixture;
 import com.tracebucket.idem.test.fixture.UserResourceFixture;
+import com.tracebucket.idem.test.util.RestRequestBuilder;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -20,12 +22,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.UUID;
 
 /**
  * Created by sadath on 30-Apr-15.
@@ -64,8 +69,13 @@ public class UserControllerTest {
     private void createUser() throws Exception {
         user = UserResourceFixture.standardUser();
         log.info("Create User : " + objectMapper.writeValueAsString(user));
-        user = restTemplate.postForObject(basePath+"/user", user, UserResource.class);
+        ResponseEntity<UserResource> responseEntity = restTemplate.exchange(basePath + "/admin/user", HttpMethod.POST, RestRequestBuilder.build(user, accessToken), UserResource.class);
+        Assert.assertNotNull(responseEntity);
+        Assert.assertNotNull(responseEntity.getBody());
+        user = responseEntity.getBody();
+        log.info("Created Group : " + objectMapper.writeValueAsString(group));
         log.info("Created User : " + objectMapper.writeValueAsString(user));
+
         Assert.assertNotNull(user);
     }
 
@@ -78,39 +88,49 @@ public class UserControllerTest {
     @Test
     public void testUpdateUser() throws Exception {
         createUser();
-        authority = restTemplate.postForObject(basePath + "/authority", AuthorityResourceFixture.tempAuthority(), AuthorityResource.class);
+
+        authority = AuthorityResourceFixture.tempAuthority();
+        ResponseEntity<AuthorityResource> authorityResponseEntity = restTemplate.exchange(basePath + "/admin/authority", HttpMethod.POST, RestRequestBuilder.build(authority, accessToken), AuthorityResource.class);
+        Assert.assertNotNull(authorityResponseEntity);
+        Assert.assertNotNull(authority = authorityResponseEntity.getBody());
+
         user.getAuthorities().add(authority);
-        restTemplate.put(basePath + "/user", user);
-        user = restTemplate.getForObject(basePath + "/user/" + user.getUsername(), UserResource.class);
-        Assert.assertNotNull(user);
-        Assert.assertNotNull(user.getUid());
-        Assert.assertNotNull(user.getAuthorities());
+
+        ResponseEntity<UserResource> responseEntity = restTemplate.exchange(basePath + "/admin/user", HttpMethod.PUT, RestRequestBuilder.build(user, accessToken), UserResource.class);
+
+        Assert.assertNotNull(responseEntity);
+        Assert.assertNotNull(user = responseEntity.getBody());
+
         Assert.assertEquals(1, user.getAuthorities().size());
     }
 
-/*    @Test
+/*
+    @Test
     public void testChangePassword() throws Exception {
         createUser();
+        accessToken = accessTokenReceiver.receive("idem-admin", "idem-admin-secret", user.getUsername(), user.getPassword());
         String newPassword = UUID.randomUUID().toString();
-        restTemplate.put(basePath + "/user/password?oldPassword="+user.getPassword()+"&newPassword="+newPassword, UserResource.class);
-        user = restTemplate.getForObject(basePath + "/user/" + user.getUsername(), UserResource.class);
-        Assert.assertNotNull(user);
-        Assert.assertNotNull(user.getUid());
+        ResponseEntity<UserResource> responseEntity = restTemplate.exchange(basePath + "/admin/user/password?oldPassword="+user.getPassword()+"&newPassword="+newPassword,HttpMethod.PUT, RestRequestBuilder.build(accessToken), UserResource.class);
+        Assert.assertNotNull(responseEntity);
+        Assert.assertNotNull(user = responseEntity.getBody());
         Assert.assertEquals(newPassword, user.getPassword());
-    }*/
+    }
+*/
 
     @Test
     public void testUserExists() throws Exception {
         createUser();
-        Boolean status = restTemplate.getForObject(basePath + "/user/" + user.getUsername() + "/exists", Boolean.class);
-        Assert.assertTrue(status);
+        ResponseEntity<Boolean> status = restTemplate.exchange(basePath + "/admin/user/" + user.getUsername() + "/exists", HttpMethod.GET, RestRequestBuilder.build(user, accessToken), Boolean.class);
+        Assert.assertTrue(status.getBody().booleanValue());
     }
 
     @Test
     public void testFindByUsername() throws Exception {
         createUser();
-        user = restTemplate.getForObject(basePath + "/user/" + user.getUsername(), UserResource.class);
-        Assert.assertNotNull(user);
+        ResponseEntity<UserResource> responseEntity = restTemplate.exchange(basePath + "/admin/user/" + user.getUsername(),HttpMethod.GET, RestRequestBuilder.build(user, accessToken), UserResource.class);
+        Assert.assertNotNull(responseEntity);
+        Assert.assertNotNull(user = responseEntity.getBody());
+        Assert.assertNotNull(user.getUsername());
         Assert.assertNotNull(user.getUid());
     }
 
@@ -118,11 +138,15 @@ public class UserControllerTest {
     public void testAddUserToGroup() throws Exception {
         createUser();
         group = GroupResourceFixture.standardGroup();
-        group = restTemplate.postForObject(basePath+"/group", group, GroupResource.class);
-        Assert.assertNotNull(group);
-        restTemplate.put(basePath + "/user/" + user.getUsername() + "/group/" + group.getName(), UserResource.class);
-        user = restTemplate.getForObject(basePath + "/user/" + user.getUsername(), UserResource.class);
-        Assert.assertNotNull(user);
+        ResponseEntity<GroupResource> groupResponseEntity = restTemplate.exchange(basePath+"/admin/group", HttpMethod.POST, RestRequestBuilder.build(group, accessToken), GroupResource.class);
+        Assert.assertNotNull(groupResponseEntity);
+        Assert.assertNotNull(groupResponseEntity.getBody().getName());
+
+
+        restTemplate.exchange(basePath + "/admin/user/" + user.getUsername() + "/group/" + group.getName(), HttpMethod.PUT, RestRequestBuilder.build(user, accessToken), UserResource.class);
+        ResponseEntity<UserResource> responseEntity = restTemplate.exchange(basePath + "/admin/user/" + user.getUsername(), HttpMethod.GET, RestRequestBuilder.build(user, accessToken), UserResource.class);
+        Assert.assertNotNull(responseEntity);
+        Assert.assertNotNull(user = responseEntity.getBody());
         Assert.assertNotNull(user.getUid());
         Assert.assertNotNull(user.getGroups());
         Assert.assertEquals(1, user.getGroups().size());
@@ -132,42 +156,50 @@ public class UserControllerTest {
     public void testRemoveUserFromGroup() throws Exception {
         createUser();
         group = GroupResourceFixture.standardGroup();
-        group = restTemplate.postForObject(basePath + "/group", group, GroupResource.class);
-        Assert.assertNotNull(group);
-        restTemplate.put(basePath + "/user/" + user.getUsername() + "/group/" + group.getName(), UserResource.class);
-        user = restTemplate.getForObject(basePath + "/user/" + user.getUsername(), UserResource.class);
-        Assert.assertNotNull(user);
+        ResponseEntity<GroupResource> groupResponseEntity = restTemplate.exchange(basePath+"/admin/group", HttpMethod.POST, RestRequestBuilder.build(group, accessToken), GroupResource.class);
+        Assert.assertNotNull(groupResponseEntity);
+        Assert.assertNotNull(group = groupResponseEntity.getBody());
+
+        ResponseEntity<UserResource> responseEntity = restTemplate.exchange(basePath + "/admin/user/" + user.getUsername() + "/group/" + group.getName(), HttpMethod.PUT, RestRequestBuilder.build(user, accessToken), UserResource.class);
+        Assert.assertNotNull(responseEntity);
+        Assert.assertNotNull(user = responseEntity.getBody());
         Assert.assertNotNull(user.getUid());
         Assert.assertNotNull(user.getGroups());
         Assert.assertEquals(1, user.getGroups().size());
-        restTemplate.delete(basePath + "/user/" + user.getUsername() + "/group/" + group.getName());
-        user = restTemplate.getForObject(basePath + "/user/" + user.getUsername(), UserResource.class);
-        Assert.assertNotNull(user);
-        Assert.assertEquals(0, user.getGroups().size());
+
+        ResponseEntity<UserResource> responseEntity1 = restTemplate.exchange(basePath + "/admin/user/" + user.getUsername() + "/group/" + group.getName(), HttpMethod.DELETE, RestRequestBuilder.build(user, accessToken), UserResource.class);
+        Assert.assertNotNull(responseEntity1);
+        Assert.assertNotNull(responseEntity1.getBody());
+        Assert.assertEquals(0, responseEntity1.getBody().getGroups().size());
     }
 
     @After
     public void tearDown() throws Exception{
         if(user != null && user.getUid() != null) {
-            restTemplate.delete(basePath + "/user/" + user.getUsername());
+            ResponseEntity<Boolean> responseEntity = restTemplate.exchange(basePath + "/admin/user/" + user.getUsername(), HttpMethod.DELETE, RestRequestBuilder.build(accessToken), Boolean.class);
+            Assert.assertNotNull(responseEntity);
+            Assert.assertTrue(responseEntity.getBody());
             try {
-                restTemplate.getForEntity(new URI(basePath + "/user/" + user.getUsername()), UserResource.class);
+                restTemplate.exchange(basePath + "/admin/user/" + user.getUsername(), HttpMethod.GET, RestRequestBuilder.build(accessToken), UserResource.class);
             } catch (HttpClientErrorException httpClientErrorException) {
                 Assert.assertEquals(HttpStatus.NOT_FOUND, httpClientErrorException.getStatusCode());
             }
         }
         if(group != null && group.getUid() != null) {
-            restTemplate.delete(basePath + "/group/" + group.getName());
+            ResponseEntity<Boolean> responseEntity = restTemplate.exchange(basePath + "/admin/group/" + group.getName(), HttpMethod.DELETE, RestRequestBuilder.build(accessToken), Boolean.class);
+            Assert.assertNotNull(responseEntity);
+            Assert.assertTrue(responseEntity.getBody());
             try {
-                restTemplate.getForEntity(new URI(basePath + "/group/" + group.getName()), GroupResource.class);
+                restTemplate.exchange(basePath + "/admin/group/" + group.getName(), HttpMethod.GET, RestRequestBuilder.build(accessToken), GroupResource.class);
             } catch (HttpClientErrorException httpClientErrorException) {
                 Assert.assertEquals(HttpStatus.NOT_FOUND, httpClientErrorException.getStatusCode());
             }
         }
         if (authority != null && authority.getUid() != null) {
-            restTemplate.delete(basePath + "/authority/" + authority.getUid());
-            try {
-                restTemplate.getForEntity(new URI(basePath + "/authority/" + authority.getUid()), AuthorityResource.class);
+            ResponseEntity<Boolean> responseEntity = restTemplate.exchange(basePath + "/admin/authority/" + authority.getUid(), HttpMethod.DELETE, RestRequestBuilder.build(accessToken), Boolean.class);
+            Assert.assertNotNull(responseEntity);
+            Assert.assertTrue(responseEntity.getBody());            try {
+                restTemplate.exchange(basePath + "/admin/authority/" + authority.getUid(), HttpMethod.GET, RestRequestBuilder.build(accessToken), AuthorityResource.class);
             } catch (HttpClientErrorException httpClientErrorException) {
                 Assert.assertEquals(HttpStatus.NOT_FOUND, httpClientErrorException.getStatusCode());
             }
