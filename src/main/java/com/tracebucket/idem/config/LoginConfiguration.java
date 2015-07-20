0457594c3ed4e6ee8457c080b6397bbd3865140d
sadath <ssmdrepo@gmail.com>
2015-07-20 14:51:54 +0530

    Changed to Version 1.8 Roles Hierarchy

--- src/main/java/com/tracebucket/idem/config/LoginConfiguration.java
+++ src/main/java/com/tracebucket/idem/config/LoginConfiguration.java
@@ -1,23 +1,40 @@
 package com.tracebucket.idem.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.RoleHierarchyVoter;
+import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 /**
  * @author FFL
  * @since 11/03/15
  */
 @Configuration
+@PropertySource(value = "classpath:rolesHierarchy.properties")
 @Order(-10)
 public class LoginConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginConfiguration.class);
+
+    @Value("${rolesHierarchy}")
+    private String rolesHierarchy;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -34,7 +51,7 @@ public class LoginConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
                 .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .authorizeRequests().anyRequest().authenticated().expressionHandler(webExpressionHandler());
         // @formatter:on
     }
 
@@ -48,5 +65,23 @@ public class LoginConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.parentAuthenticationManager(authenticationManager);
         auth.userDetailsService(userDetailsManager);
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy(rolesHierarchy);
+        return roleHierarchy;
+    }
+
+    @Bean
+    public RoleVoter roleVoter() {
+        return new RoleHierarchyVoter(roleHierarchy());
+    }
+
+    private SecurityExpressionHandler<FilterInvocation> webExpressionHandler() {
+        DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler = new DefaultWebSecurityExpressionHandler();
+        defaultWebSecurityExpressionHandler.setRoleHierarchy(roleHierarchy());
+        return defaultWebSecurityExpressionHandler;
     }
 }
