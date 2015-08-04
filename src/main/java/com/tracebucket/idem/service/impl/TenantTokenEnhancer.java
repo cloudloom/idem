e403b808742079b85193b9880505e3d34a2d8ee8
sadath <ssmdrepo@gmail.com>
2015-08-04 18:34:15 +0530

    Changed to Version 2.4 Merged branches IDEM-4 and IDEM-7

--- src/main/java/com/tracebucket/idem/service/impl/TenantTokenEnhancer.java
+++ src/main/java/com/tracebucket/idem/service/impl/TenantTokenEnhancer.java
@@ -1,6 +1,9 @@
 package com.tracebucket.idem.service.impl;
 
+import com.tracebucket.idem.domain.Client;
+import com.tracebucket.idem.domain.Tenant;
 import com.tracebucket.idem.domain.User;
+import com.tracebucket.idem.repository.jpa.ClientRepository;
 import com.tracebucket.idem.repository.jpa.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
@@ -9,7 +12,9 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author FFL
@@ -22,14 +27,35 @@ public class TenantTokenEnhancer implements TokenEnhancer{
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ClientRepository clientRepository;
+
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         String userName = authentication.isClientOnly() ? null : authentication.getName();
         User user = userRepository.findByUsername(userName);
 
+        String clientId = authentication.getOAuth2Request().getClientId();
+        Client client = clientRepository.findByClientId(clientId);
+
+        Tenant tenant = client.getTenant();
+
         Map<String, Object> tenantInformation = user.getTenantInformation();
 
-        ((DefaultOAuth2AccessToken)accessToken).setAdditionalInformation(tenantInformation);
+        if(tenantInformation != null && tenantInformation.containsKey("TENANT_ID")) {
+            Set<Tenant> tenants = (Set<Tenant>)tenantInformation.get("TENANT_ID");
+            if(tenants != null && tenants.size() > 0) {
+                for(Tenant tenant1 : tenants) {
+                    if(tenant1.getName().equals(tenant.getName())) {
+                        Map<String, Object> additionalInfo = new HashMap<String, Object>();
+                        additionalInfo.put("TENANT_ID", tenant1.getName());
+                        ((DefaultOAuth2AccessToken)accessToken).setAdditionalInformation(additionalInfo);
+                        return accessToken;
+
+                    }
+                }
+            }
+        }
         return accessToken;
     }
 }
