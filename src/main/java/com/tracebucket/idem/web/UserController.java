6c7c86ac59d99edc4bcc0643c54a4175da0af43f
ffazil <firoz.fazil@gmail.com>
2015-09-04 17:31:34 +0530

    Implemented Oauth Token revoke

--- src/main/java/com/tracebucket/idem/web/UserController.java
+++ src/main/java/com/tracebucket/idem/web/UserController.java
@@ -1,8 +1,9 @@
 package com.tracebucket.idem.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import com.tracebucket.idem.service.impl.JpaTokenStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -14,8 +15,18 @@ import java.security.Principal;
 @SessionAttributes("authorizationRequest")
 public class UserController {
 
+    @Autowired
+    private JpaTokenStore jpaTokenStore;
+
     @RequestMapping("/user")
     public Principal user(Principal user) {
         return user;
     }
+
+    @RequestMapping(value = "/oauth/token/revoke", method = RequestMethod.POST)
+    public @ResponseBody void create(@RequestParam("token") String value) throws InvalidClientException {
+        jpaTokenStore.removeAccessToken(value);
+    }
+
+
 }
